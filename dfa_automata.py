50373f58530ede6a3fef778c065091d27cee2282
ShaneMatsushima <ShaneMatsushima@gmail.com>
2021-11-11 13:06:07 -0800

    dfa implement to main script

--- dfa_automata.py
+++ dfa_automata.py
@@ -1,15 +1,17 @@
 from automata.fa.dfa import DFA
+from dfa.dfa_dict_1 import trans, states, finals, alphabet, initial
 
-dfa = DFA(
-        states={'q0', 'q1', 'q2'},
-        input_symbols={'0', '1'},
-        transitions={
-            'q0':{'0':'q0', '1':'q1'},
-            'q1':{'0': 'q0', '1':'q2'},
-            'q2':{'0': 'q2', '1':'q1'} 
-        },
-        initial_state='q0',
-        final_states={'q1'}
+new_dfa = DFA(
+        states= states,
+        input_symbols= alphabet,
+        transitions=trans,
+        initial_state=initial,
+        final_states=finals
 )
+
+print(new_dfa.accepts_input('01'))
+
+
+
 
 
