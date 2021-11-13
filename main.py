76c28822b22950a795fe8a84f8ed931c497588db
ShaneMatsushima <ShaneMatsushima@gmail.com>
2021-11-12 19:11:04 -0800

    output text implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from automata.fa.dfa import DFA
+import os
 
 
 #TODO grab variables of dfa from text file of dfa
@@ -16,8 +17,12 @@ initial = ''
 finals = {}
 transitions = {}
 
-for lines in input_dfa:
-    exec(lines)
+name = input_dfa[0]
+input_dfa_append = input_dfa[1:]
+
+
+for lines in input_dfa_append:
+    exec(lines.strip())
 
 
 #TODO implement grabbed variables to create dfa
@@ -40,13 +45,42 @@ test = f.readlines()
 f.close()
 
 #test each test case from file
+result = []
+step_list = []
+
 for cases in test:
-    print(new_dfa.accepts_input(cases.strip()))
+    step_list.append(new_dfa.read_input_stepwise(cases.strip()))
+    result.append(new_dfa.accepts_input(cases.strip()))
+
 
 #TODO grab case results and write to an output file 
 # (or terminal if not enough time to implement)
 
-# out = open('put-output-results-file-here', 'w')
+result_name = 'test_result_' + name + ".txt"
+
+# delete file if it already exists
+if os.path.exists(result_name):
+        os.remove(result_name)
+
+#create file and write results to it
+q = open(result_name, 'w')
+
+for i in range(len(result)):
+
+    #writes bool result of test case
+    q.write(str(result[i]) + ":" + " " + str(test[i]))
+
+    #write states reached each for each state accepted
+    if result[i] == True:
+        q.write("states visited: ")
+        for step in step_list[i]:
+            q.write(str(step) + "->")
+        q.write("accept\n")
+    else:
+        q.write("States visited: Rejected\n")
+
+q.close()
+
 
 
 
